6070fb8bae94540ca115c156a325267d6078be63
Pedro Henrique Ribeiro Custódio <pedrohenrique11112004@hotmail.com>
2025-06-14 18:15:01 -0300

    feat(ProjetoJWT) Adicionado as pastas príncipais do projeto

--- demo/src/main/java/com/example/demo/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.security.core.userdetails.UserDetails;
-
-public interface UserRepository extends JpaRepository<UserModel, Long> {
-    UserDetails findByLogin(String role);
-
-}
