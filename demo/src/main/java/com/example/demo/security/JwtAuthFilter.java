3923ed55226c65df5a927a3662c14fa136c48dd2
Pedro Henrique Ribeiro Custódio <pedrohenrique11112004@hotmail.com>
2025-06-14 19:01:07 -0300

    feat(ProjetoJWT) Adicionado a classe SecurityConfig

--- demo/src/main/java/com/example/demo/security/JwtAuthFilter.java
+++ demo/src/main/java/com/example/demo/security/JwtAuthFilter.java
@@ -22,7 +22,7 @@ public class JwtAuthFilter extends OncePerRequestFilter {
 
     @Autowired
     private CustomUserDetailsService customUserDetailsService;
-    
+
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
         String token = getJwtFromRequest(request);
